b8f6b5383ea55758eb8f2836982c51db34ca77f7
Administrator <administrator@AdminisatorsMBP.fritz.box>
2020-07-16 13:26:45 +0200

    events.js

--- events.js
+++ events.js
@@ -1,3 +1,16 @@
 document.querySelector("button").addEventListener("click", (e)=> {
     // do stuff here.
   })
+
+
+    window.onload = function () {
+      document
+        .querySelector(".say-hi-button")
+        .addEventListener("click", function () {
+          // here you can change also a-scene or a-entity properties, like
+          // changing your 3D model source, size, position and so on
+          // or you can just open links, trigger actions...
+          alert("Hi there!");
+        });
+    };
+ 
