18ae5fa67d456a28d4387b520570a4a546f0ae99
Administrator <administrator@AdminisatorsMBP.fritz.box>
2020-07-16 13:48:10 +0200

    .klnydfg

--- events.js
+++ events.js
@@ -6,6 +6,7 @@
           // changing your 3D model source, size, position and so on
           // or you can just open links, trigger actions...
           alert("Hi there!");
+          console.log("Click");
         });
     };
  
